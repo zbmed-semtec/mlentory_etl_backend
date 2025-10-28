24b99b663f199f0d9c2f935153c703f6a1196874
bit2424 <nelsonquinones2424@gmail.com>
2025-10-28 15:07:26 +0000

    :sparkles: Integrate OpenML assets into ETL repository

--- etl/repository.py
+++ etl/repository.py
@@ -7,7 +7,7 @@ This is the main entrypoint for the Dagster instance.
 from dagster import repository, load_assets_from_modules
 
 from etl.assets import hf_extraction as hf_assets_module
-
+from etl.assets import openml_extraction as openml_assets_module
 
 @repository
 def mlentory_etl_repository():
@@ -18,5 +18,6 @@ def mlentory_etl_repository():
         list: List of jobs and schedules
     """
     hf_assets = load_assets_from_modules([hf_assets_module])
-    return [*hf_assets]
+    openml_assets = load_assets_from_modules([openml_assets_module])
+    return [*hf_assets, *openml_assets]
 
