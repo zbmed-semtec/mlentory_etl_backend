35822b41dc30feb5a5f65c0f16e88af2d1f18285
bit2424 <nelsonquinones2424@gmail.com>
2025-11-08 17:01:15 +0000

    :sparkles: Add HuggingFace loading assets to the ETL repository

--- etl/repository.py
+++ etl/repository.py
@@ -8,6 +8,7 @@ from dagster import repository, load_assets_from_modules
 
 from etl.assets import hf_extraction as hf_extraction_module
 from etl.assets import hf_transformation as hf_transformation_module
+from etl.assets import hf_loading as hf_loading_module
 from etl.assets import openml_extraction as openml_assets_module
 from etl.assets import ai4life_extraction as ai4life_assets_module
 
@@ -21,7 +22,8 @@ def mlentory_etl_repository():
     """
     hf_extraction_assets = load_assets_from_modules([hf_extraction_module])
     hf_transformation_assets = load_assets_from_modules([hf_transformation_module])
+    hf_loading_assets = load_assets_from_modules([hf_loading_module])
     openml_assets = load_assets_from_modules([openml_assets_module])
     ai4life_assets = load_assets_from_modules([ai4life_assets_module])
-    return [*hf_extraction_assets, *hf_transformation_assets, *openml_assets, *ai4life_assets]
+    return [*hf_extraction_assets, *hf_transformation_assets, *hf_loading_assets, *openml_assets, *ai4life_assets]
 
