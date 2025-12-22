98baf5dd978f18397f9ffb66375d866a8fabbbda
rohitharavinder <rohitha0112@gmail.com>
2025-12-22 06:21:57 +0000

    ✨ Refactor OpenML keyword client

--- etl_extractors/openml/clients/__init__.py
+++ etl_extractors/openml/clients/__init__.py
@@ -2,7 +2,7 @@ from .openml_runs_client import OpenMLRunsClient
 from .openml_datasets_client import OpenMLDatasetsClient
 from .openml_flows_client import OpenMLFlowsClient
 from .openml_tasks_client import OpenMLTasksClient
-from .keyword_client import OpenMLKeywordClient
+from .openml_keyword_client import OpenMLKeywordClient
 
 __all__ = [
     "OpenMLRunsClient",
