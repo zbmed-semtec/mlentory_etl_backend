2eb8164cfc892d8f15785bc288ed2c764467cab2
ShriSuha <suhasinivenkatesh.official@gmail.com>
2026-09-01 07:22:01 +0000

    Remove duplicate Kaggle clients __int__.py typo file

--- etl_extractors/kaggle/clients/__int__.py
+++
@@ -1,9 +0,0 @@
-from .models_client import KaggleModelClient
-from .instances_client import KaggleInstancesClient
-from .licenses_client import KaggleLicenseClient
-from .keywords_client import KaggleKeywordsClient
-from .frameworks_client import KaggleFrameworkClient
-from .sharedby_client import KaggleSharedByClient
-
-__all__ = ["KaggleModelClient", "KaggleInstancesClient", "KaggleLicenseClient", 
-           "KaggleKeywordsClient","KaggleFrameworkClient","KaggleSharedByClient"]
